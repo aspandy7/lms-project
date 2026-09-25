fc3139d23e8c5d36868185acca301c177c249a44
Alisher Melzetdinov <alishermelzetdinov@MacBook-Air-Alisher.local>
2026-09-25 14:30:53 +0300

    Заготовлен проект для деплоя

--- lms-frontend/services/api.ts
+++ lms-frontend/services/api.ts
@@ -1,5 +1,6 @@
 
-const BASE_URL = 'http://localhost:8000/api/v1';
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1';
 
 // Helper to get token
 const getToken = () => localStorage.getItem('access_token');
